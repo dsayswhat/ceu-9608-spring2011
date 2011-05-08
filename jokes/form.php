f9c7eb0d7cbedce62900d6c66804c19fbe4db42a
Darren Petersen <dsayswhat@gmail.com>
2011-05-07 20:29:11 -0700

    sample code for radios and checkboxes

--- jokes/form.php
+++ jokes/form.php
@@ -18,6 +18,22 @@
         </option>
       </select>
     </p>
+	 
+	<p>
+		<ul>
+			<li><input type="radio" name="myRadio" value="1" />radio value 1</li>
+			<li><input type="radio" name="myRadio" value="2" />radio value 2</li>	
+			<li><input type="radio" name="myRadio" value="3" />radio value 3</li>	
+		</ul>
+	</p>
+	<p>
+		<ul>
+			<li><input type="checkbox" name="myCbx" value="1" />checkbox value 1</li>
+			<li><input type="checkbox" name="myCbx" value="2" />checkbox value 2</li>	
+			<li><input type="checkbox" name="myCbx" value="3" />checkbox value 3</li>	
+		</ul>
+	</p>
+	
     <input type="submit" value="Save joke" />
     <input type="hidden" name="jokeid" value="" />
   </fieldset>
