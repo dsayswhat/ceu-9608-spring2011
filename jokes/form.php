3cbcfa98b9ea4a0c4180b6e273e8350ed36d6333
Darren Petersen <dsayswhat@gmail.com>
2011-05-07 20:34:18 -0700

    labels for radios and checkboxes

--- jokes/form.php
+++ jokes/form.php
@@ -21,16 +21,16 @@
 	 
 	<p>
 		<ul>
-			<li><input type="radio" name="myRadio" value="1" />radio value 1</li>
-			<li><input type="radio" name="myRadio" value="2" />radio value 2</li>	
-			<li><input type="radio" name="myRadio" value="3" />radio value 3</li>	
+			<li><input id="rdo1" type="radio" name="myRadio" value="1" /><label for="rdo1">radio value 1</label></li>
+			<li><input id="rdo2" type="radio" name="myRadio" value="2" /><label for="rdo2">radio value 2</label></li>	
+			<li><input id="rdo3" type="radio" name="myRadio" value="3" /><label for="rdo3">radio value 3</label></li>	
 		</ul>
 	</p>
 	<p>
 		<ul>
-			<li><input type="checkbox" name="myCbx" value="1" />checkbox value 1</li>
-			<li><input type="checkbox" name="myCbx" value="2" />checkbox value 2</li>	
-			<li><input type="checkbox" name="myCbx" value="3" />checkbox value 3</li>	
+			<li><input id="cbx1" type="checkbox" name="myCbx" value="1" /><label for="cbx1">checkbox value 1</label></li>
+			<li><input id="cbx2" type="checkbox" name="myCbx" value="2" /><label for="cbx2">checkbox value 2</label></li>	
+			<li><input id="cbx3" type="checkbox" name="myCbx" value="3" /><label for="cbx3">checkbox value 3</label></li>	
 		</ul>
 	</p>
 	
